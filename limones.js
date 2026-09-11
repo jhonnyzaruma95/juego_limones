6f9eb7cbc072d2556ee43981bf9ced6fa42a5404
jhonnyzaruma95 <jhonnyz1995@hotmail.com>
2026-09-11 17:02:02 -0500

    agregar funciones dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,2 +1,22 @@
-let canvas=document.getElementById("area de juego");
+let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+const Altura_Suelo=40;
+const Altura_Personaje=60;
+const Ancho_Personaje=40;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function dibujarSuelo(){
+    ctx.fillStyle="red";
+    ctx.fillRect(0,canvas.height-Altura_Suelo,canvas.width,Altura_Suelo);
+
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle="yellow";
+    ctx.fillRect(canvas.width/2,canvas.height-(Altura_Suelo+Altura_Personaje),Ancho_Personaje,Altura_Personaje);
+    
+}
