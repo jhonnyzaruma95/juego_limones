60534eb43e4431fea016a9a4bffba70dc52b99d4
jhonnyzaruma95 <jhonnyz1995@hotmail.com>
2026-09-12 23:08:38 -0500

    agregar bajar limon

--- limones.js
+++ limones.js
@@ -1,38 +1,60 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-const Altura_Suelo=40;
-const Altura_Personaje=60;
-const Ancho_Personaje=40;
+const ALTURA_SUELO=40;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
+
 let personajeX=canvas.width/2;
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 function dibujarSuelo(){
     ctx.fillStyle="red";
-    ctx.fillRect(0,canvas.height-Altura_Suelo,canvas.width,Altura_Suelo);
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
 
 }
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(personajeX,canvas.height-(Altura_Suelo+Altura_Personaje),Ancho_Personaje,Altura_Personaje);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
     
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
-    
+}
+//moverDerecha
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
 }
 function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 
 }
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="pink";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+
+}
+
+function bajarLimon(){
+    limonY=limonY+10;
+    actualizarPantalla();
 }
