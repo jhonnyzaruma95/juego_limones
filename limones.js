5c1db8a23aefc24b74ba61130b752f77124c11e2
jhonnyzaruma95 <jhonnyz1995@hotmail.com>
2026-09-13 16:33:03 -0500

    posicion aleatorio del limon

--- limones.js
+++ limones.js
@@ -15,7 +15,7 @@ let limonY=0;
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
-    dibujarLimon();
+    aparecerLimon();
 }
 function dibujarSuelo(){
     ctx.fillStyle="red";
@@ -32,13 +32,13 @@ function dibujarPersonaje(){
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
-    detectarColision();
 }
+
 //moverDerecha
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
-    detectarColision();
+    
 }
 function actualizarPantalla(){
     limpiarCanva();
@@ -60,12 +60,27 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY=limonY+10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function detectarColision(){
     if(limonX + ANCHO_LIMON > personajeX && limonX < personajeX + ANCHO_PERSONAJE
         && limonY + ALTURA_LIMON > personajeY && limonY < personajeY + ALTURA_PERSONAJE){
-        alert("ATRAPADO!!");
+        //alert("ATRAPADO!!");
+        aparecerLimon();
 
     }
+}
+
+function probarAleatorio(){
+    let aleatorio=generarAleatorio(10,80);
+    console.log(aleatorio);
+    
+}
+
+function aparecerLimon(){
+    limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
+    limonY=0;
+    actualizarPantalla();
+
 }
---
+++ utils.js
@@ -0,0 +1,9 @@
+function generarAleatorio(min,max){
+    let random=Math.random();//0-1
+    //Ejemplo: max es 600, minimo es 5
+    let numero=random*(max-min);//0-max 0-595
+    let numeroEntero = Math.ceil(numero);
+    //Ejemplo: 0
+    numeroEntero=numeroEntero+min;//5-600
+    return numeroEntero
+}
