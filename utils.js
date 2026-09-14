1e489cec4dc06feb775abae6c9a9c8bc1dfa0d7d
jhonnyzaruma95 <jhonnyz1995@hotmail.com>
2026-09-13 21:22:41 -0500

    puntaje y vidas del limon

--- utils.js
+++ utils.js
@@ -7,3 +7,7 @@ function generarAleatorio(min,max){
     numeroEntero=numeroEntero+min;//5-600
     return numeroEntero
 }
+function mostrarEnSpan(idSpan,valor){
+    let componente=document.getElementById(idSpan);
+    componente.textContent=valor;
+}
